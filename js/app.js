22b694b9ed1b9701b7a1c264a72c77362f1e6b0b
Swapnil Wadhankar <swapnilwadhankar12@gmail.com>
2020-07-23 19:22:15 +0530

    Simple tasklist using JS

---
+++ js/app.js
@@ -0,0 +1,64 @@
+const form = document.querySelector("#task-form");
+const taskList = document.querySelector(".collection");
+const clearBtn = document.querySelector(".clear-task");
+const taskInput = document.querySelector("#task");
+const filter = document.querySelector("#filter");
+
+loadEventListener();
+
+function loadEventListener() {
+  form.addEventListener("submit", addTask);
+  taskList.addEventListener("click", removeTask);
+  clearBtn.addEventListener("click", removeAll);
+  filter.addEventListener("keyup", filterList);
+}
+function addTask(e) {
+  if (taskInput.value === "") {
+    alert("Add a task");
+  }
+  const li = document.createElement("li");
+  li.className = "collection-item";
+  li.appendChild(document.createTextNode(taskInput.value));
+  const link = document.createElement("a");
+  link.className = "delete-item secondary-content";
+  link.innerHTML = '<i class = "fa fa-remove" ></i>';
+  li.appendChild(link);
+  taskList.appendChild(li);
+  storeTaskInLocalStorage(taskInput.value);
+  taskInput.value = "";
+
+  e.preventDefault();
+}
+function storeTaskInLocalStorage(task) {
+  let tasks;
+  if (localStorage.getItem("tasks") === null) {
+    tasks = [];
+  } else {
+    tasks = JSON.parse(localStorage.getItem("tasks"));
+  }
+  tasks.push(task);
+  localStorage.setItem("tasks", JSON.stringify(tasks));
+}
+function removeTask(e) {
+  if (e.target.parentElement.classList.contains("delete-item")) {
+    if (confirm("Are you sure?")) {
+      e.target.parentElement.parentElement.remove();
+    }
+  }
+}
+
+function removeAll(e) {
+  taskList.innerHTML = "";
+}
+
+function filterList(e) {
+  const text = e.target.value.toLowerCase();
+  document.querySelectorAll(".collection-item").forEach(function(task) {
+    const item = task.firstChild.textContent;
+    if (item.toLowerCase().indexOf(text) != -1) {
+      task.style.display = "block";
+    } else {
+      task.style.display = "none";
+    }
+  });
+}
